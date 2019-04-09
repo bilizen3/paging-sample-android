ec1590b0ce3ec0fc8bc65ec24bb4843ce58326fe
Bill Flores <bilizen3@gmail.com>
2019-04-08 22:56:57 -0500

    update viewmodel

--- app/src/main/java/com/flores/paging_sample_android/viewmodel/PagingViewModel.kt
+++ app/src/main/java/com/flores/paging_sample_android/viewmodel/PagingViewModel.kt
@@ -1,14 +1,8 @@
 package com.flores.paging_sample_android.viewmodel
-
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import androidx.paging.PagedList
-import com.flores.paging_sample_android.data.model.ResultsItem
 import com.flores.paging_sample_android.repository.PagingRepository
-import com.flores.paging_sample_android.repository.SearchResult
-import com.flores.paging_sample_android.utils.Status
 
 class PagingViewModel(
     private val repository: PagingRepository
@@ -16,25 +10,22 @@ class PagingViewModel(
 
     private val searchtext = MutableLiveData<String>()
 
-    private var searchTotalLiveData: LiveData<SearchResult> =
+    private var searchTotalLiveData =
         Transformations.map(searchtext) {
             repository.searchTotal(it)
         }
 
-    val resultCountTotal: LiveData<Int> =
-        Transformations.switchMap(searchTotalLiveData) {
-            it.countTotal
-        }
+    val resultCountTotal = Transformations.switchMap(searchTotalLiveData) {
+        it.countTotal
+    }
 
-    val resultItems: LiveData<PagedList<ResultsItem>> =
-        Transformations.switchMap(searchTotalLiveData) {
-            it.results
-        }
+    val resultItems = Transformations.switchMap(searchTotalLiveData) {
+        it.results
+    }
 
-    val status: LiveData<Status> =
-        Transformations.switchMap(searchTotalLiveData) {
-            it.status
-        }
+    val status = Transformations.switchMap(searchTotalLiveData) {
+        it.status
+    }
 
     fun searchMovie(text: String) {
         searchtext.postValue(text)
